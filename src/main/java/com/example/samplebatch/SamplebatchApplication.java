b3ecd19fda22c3302e57726d872a2d6d1284fde7
kicok <kicoker@gmail.com>
2025-08-29 23:23:55 +0900

    project setting & Configure Setting

---
+++ src/main/java/com/example/samplebatch/SamplebatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplebatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplebatchApplication.class, args);
+	}
+
+}
