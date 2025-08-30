97e4ecbeb88a30c12d9a7c09249aacfb900e5fe8
kicok <kicoker@gmail.com>
2025-08-30 15:50:33 +0900

    batch 정의

---
+++ src/main/java/com/example/samplebatch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.example.samplebatch.repository;
+
+import com.example.samplebatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
